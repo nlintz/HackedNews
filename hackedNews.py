6f1b07e7bdf823c0d8de902eeca8b2826370c8cd
Nathan Lintz <nlintz@gmail.com>
2013-04-15 23:40:28 -0400

    fixed mongo url

--- hackedNews.py
+++ hackedNews.py
@@ -1,10 +1,8 @@
 import os
 import os.path
+from urlparse import urlsplit
 import sys
 from pymongo import MongoClient
-import dbConfig
-sys.path.append( "PickleMonger" )
-from PickleMonger.PickleMonger import PickleMonger
 import tornado.ioloop
 import tornado.web
 import scrape
@@ -15,54 +13,23 @@ import ui_methods
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+    "cookie_secret": "RobotsBeware",
     "login_url": "/login",
     "xsrf_cookies": True,
 }
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-    	# mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017')
-    	# print os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017')
-    	# db_name = "bayesDict"
-    	# connection = pymongo.Connection(mongo_url)
-    	# ham = connection.Ham
-    	# test = connection.Spam
-
-    	dbConfig.dbSetup()
     	titleLinkAssoc = scrape.scrapeHN()
         self.render("home.html",
-        titleLinks = titleLinkAssoc,
-        voteHandler = "/static/javascript/vote-handler.js" )
+        titleLinks = titleLinkAssoc
+        )
     
-    def post(self):
-		PM = PickleMonger('bayesDict.dat')
-
-		args = [self.get_arguments("articleName"),self.get_arguments("voteType")]
-		articleName = args[0][0]
-		voteType = str(args[1][0])
-		query = helpers.formatQuery(articleName)
-		ham = PM.read_allInstances('ham')
-		spam = PM.read_allInstances('spam')
-		if voteType == "up":
-			for word in query:
-				ham.wordCountDict.setdefault(word, 0)
-				ham.wordCountDict[word] = ham.wordCountDict[word] + 1
-
-		if voteType == "down":
-			for word in query:
-				spam.wordCountDict.setdefault(word, 0)
-				spam.wordCountDict[word] = spam.wordCountDict[word] + 1
-
-		PM.updateObject(ham)
-		PM.updateObject(spam)
-
-		print "spam liklihood: " + str(naiveBayes.naiveBayes(spam.wordCountDict, ham.wordCountDict, query))
-
 class Ham(tornado.web.RequestHandler):
 	def get(self):
 		mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017')
-		db_name = "bayesDict"
+		parsed = urlsplit(mongo_url)
+		db_name = parsed.path[1:]
 		client = MongoClient(mongo_url)
 		db = client.db_name
 		ham = db.Ham
@@ -116,32 +83,11 @@ class Spam(tornado.web.RequestHandler):
 
 class Filter(tornado.web.RequestHandler):
 	def get(self):
-		self.rnder("spam.html")
-
-class PosteriorHandler(tornado.web.RequestHandler):
-	def get(self):
-		PM = PickleMonger('bayesDict.dat')
-		ham = PM.read_allInstances('ham').wordCountDict
-		spam = PM.read_allInstances('spam').wordCountDict
-		
-		titleProbDict = {}
-		titles = helpers.getTitles()
-		for title in titles:
-			key = ''.join(title.split())
-			query = helpers.formatQuery(title)
-			titleProbDict[key] = str(naiveBayes.naiveBayes(spam, ham, query))
-
-		self.write(
-			json.dumps(
-				titleProbDict
-				)
-			)
-
+		self.render("spam.html")
 
 
 handlers = [
     (r"/", MainHandler),
-    (r"/posterior", PosteriorHandler),
     (r"/ham", Ham),
     (r"/spam", Spam),
     (r"/filter", Filter),
