32bec978fa02deb50cbe4358feabd67f2382a75b
Nathan Lintz <nlintz@gmail.com>
2013-04-22 15:01:40 -0400

    added support for metafilter

--- static/javascript/jquery.vote-handler.js
+++ static/javascript/jquery.vote-handler.js
@@ -1,15 +1,21 @@
 $.fn.voteHandler = function() {
 	var voteHandler = function(){
+
+	$('a').bind("click", function() {
+		upVotedArticle = $(this).data('title');
+		voteAjax(upVotedArticle, "up");
+		});
+
 	$('.btn-up').bind("click", function() {
 	upVotedArticle = $(this).data('title');
-  	voteAjax(upVotedArticle, "up");
+	voteAjax(upVotedArticle, "up");
 		});
 
 	$('.btn-down').click(function() {
 	downVotedArticle = $(this).data('title');
-  	voteAjax(downVotedArticle, "down");
+	voteAjax(downVotedArticle, "down");
 		});
-	}
+	};
 
 	function voteAjax(articleName, voteType){
 		if(voteType=="up"){
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -3,9 +3,12 @@ require(["jquery", "jquery.vote-handler", "color-handler", "postTitle"], functio
     $().voteHandler(); //Binds vote buttons to ajax calls
     colorHandler.colorLabels(); //Sets up colors when pages loads
     $('.btn-up').bind("click", function() {
-    	colorHandler.colorLabels();
+		colorHandler.colorLabels();
 	});
 	$('.btn-down').bind("click", function() {
-    	colorHandler.colorLabels();
+		colorHandler.colorLabels();
+	});
+	$('a').bind("click", function() {
+		colorHandler.colorLabels();
 	});
 });
